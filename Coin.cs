f1ff14763dd8f3a5c805d47afa5a1cd3ff47ee18
LeBuuToan <98265816+LeBuuToan@users.noreply.github.com>
2022-02-04 20:41:52 +0700

    Scripts

---
+++ Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float moveSpeed;
+
+    CoinMove coinMoveScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        coinMoveScript = gameObject.GetComponent<CoinMove>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "CoinDetector")
+        {
+            coinMoveScript.enabled = true;
+        }
+    }
+}
